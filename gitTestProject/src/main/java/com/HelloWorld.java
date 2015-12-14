0afec340ee8976b6efc64d3b254daab9a2229008
Dmitriy <seaadevil@gmail.com>
2015-12-14 15:30:15 +0200

    first commit

---
+++ gitTestProject/src/main/java/com/HelloWorld.java
@@ -0,0 +1,10 @@
+package com;
+
+/**
+ * Created by Dmitry on 14.12.2015.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+}
